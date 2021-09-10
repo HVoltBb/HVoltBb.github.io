d43a327325eac73277498edd7a7945864fb1428b
hyena <eidotog@gmail.com>
2021-09-09 23:18:33 -0500

    upd

--- js/bks.js
+++ js/bks.js
@@ -7,6 +7,54 @@ data=
     "cap_p2": "<p><span class=\"label label-primary\">Reading</span></p>"
   },
   {
+    "img_link": "<img src=\"/pics/book_accounting.jpeg\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.mheducation.com/highered/product/fundamental-accounting-principles-wild-shaw/M9781259916960.html\">Fundamental Accounting Principles</a></h3>",
+    "cap_p1": "<p>A good book to get started. Very easy to read. However, you do need a good instructor to help you study the material.</p>",
+    "cap_p2": "<p><span class=\"label label-primary\">Reading</span></p>"
+  },
+  {
+    "img_link": "<img src=\"/pics/book_linear_algebra.png\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://math.mit.edu/~gs/linearalgebra/\">Introduction to Linear Algebra</a></h3>",
+    "cap_p1": "<p>Great college level linear algebra textbook. Easy to read. It is not rated any higher because this subject is very dry and boring.</p>",
+    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>"
+  },
+  {
+    "img_link": "<img src=\"/pics/book_time_series.jpg\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.springer.com/gp/book/9783319524511\">Time Series Analysis and Its Applications</a></h3>",
+    "cap_p1": "<p>Great college/graduate level time series analysis book. Easy to read. Great introductory book. The downside is that almost none of the models in this book has application value in real world problems.</p>",
+    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>"
+  },
+  {
+    "img_link": "<img src=\"/pics/book_coin.jpeg\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://oxford.universitypressscholarship.com/view/10.1093/0198774508.001.0001/acprof-9780198774501\">Likelihood-Based Inference in Cointegrated Vector Autoregressive Models</a></h3>",
+    "cap_p1": "<p>A classic text on co-integration. It has all the necessary formulae for you to implement you own program for co-integration tests. Highly recommended.</p>",
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+  },
+  {
+    "img_link": "<img src=\"/pics/book_math_stats.jpeg\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.cengage.com/c/mathematical-statistics-and-data-analysis-3e-rice/9780534399429/\">Mathematical Statistics and Data\u00a0Analysis by John A. Rice</a></h3>",
+    "cap_p1": "<p>Introductory book on mathematical statistics. It is the book recommended by my advisor on this subject. Easy to read.</p>",
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+  },
+  {
+    "img_link": "<img src=\"/pics/book_stats_inf.jpg\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.amazon.com/Statistical-Inference-George-Casella/dp/0534243126\">Statistical Inference by Casella and Berger</a></h3>",
+    "cap_p1": "<p>Classic graduate level text on statistical inference. Need to say no more.</p>",
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+  },
+  {
+    "img_link": "<img src=\"/pics/book_linear_models.jpg\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.routledge.com/A-Primer-on-Linear-Models/Monahan/p/book/9781420062014\">A Primer on Linear Models by John F. Monahan</a></h3>",
+    "cap_p1": "<p>A good text to read after taking linear algebra. May be read along with statistical inference. The text is a bit dry and boring though.</p>",
+    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>"
+  },
+  {
+    "img_link": "<img src=\"/pics/book_rudin.jpg\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.amazon.com/Principles-Mathematical-Analysis-International-Mathematics/dp/007054235X\">Principles of Mathematical Analysis</a></h3>",
+    "cap_p1": "<p>Baby Rudin. This book is better studied with a good instructor. There are so many hidden inside stories in this book. An instructor who is only good at copying notes from his/her notepad to the blackboard is no good.</p>",
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+  },
+  {
     "img_link": "<img src=\"/pics/#\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"#\">...</a></h3>",
     "cap_p1": "<p>...</p>",
