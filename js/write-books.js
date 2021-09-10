d92261b152af2ece87b9948dd82e85b85f0c22ff
hyena <eidotog@gmail.com>
2021-09-10 12:48:17 -0500

    style

--- js/write-books.js
+++ js/write-books.js
@@ -5,7 +5,7 @@ var text_cap = '<div class="caption">';
 var text = '';
 for(let i=0; i < len; i++){
   text +=text_pre + data[i].img_link + text_cap + data[i].cap_link 
-      + data[i].cap_p1 + data[i].cap_p2 + text_post;
+      + data[i].cap_p2 + data[i].cap_p1 + text_post;
 }
 
 document.getElementById('book_shelf').innerHTML = text;
