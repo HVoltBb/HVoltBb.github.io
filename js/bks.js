01dbd92c1a80fd667ec0cc6072a73ab1baf59bde
hyena <eidotog@gmail.com>
2021-09-10 17:51:01 -0500

    upd

--- js/bks.js
+++ js/bks.js
@@ -4,96 +4,168 @@ data=
     "img_link": "<img src=\"/pics/bookx_l_sql.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://www.oreilly.com/library/view/learning-sql-3rd/9781492057604/\">Learning SQL by Alan Beaulieu</a></h3>",
     "cap_p1": "<p>Nice introductory SQL book to prepare you in the direction of data science.</p>",
-    "cap_p2": "<p><span class=\"label label-primary\">Reading</span></p>"
+    "cap_p2": "<p><span class=\"label label-primary\">Reading</span></p>",
+    "sub": "Tech"
   },
   {
     "img_link": "<img src=\"/pics/bookx_accounting.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://www.mheducation.com/highered/product/fundamental-accounting-principles-wild-shaw/M9781259916960.html\">Fundamental Accounting Principles</a></h3>",
     "cap_p1": "<p>A good book to get started. Very easy to read. However, you do need a good instructor to help you study the material.</p>",
-    "cap_p2": "<p><span class=\"label label-primary\">Reading</span></p>"
+    "cap_p2": "<p><span class=\"label label-primary\">Reading</span></p>",
+    "sub": "Biz"
   },
   {
     "img_link": "<img src=\"/pics/bookx_linear_algebra.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://math.mit.edu/~gs/linearalgebra/\">Introduction to Linear Algebra</a></h3>",
     "cap_p1": "<p>Great college level linear algebra textbook. Easy to read. It is not rated any higher because this subject is very dry and boring.</p>",
-    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>"
+    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>",
+    "sub": "Math"
   },
   {
     "img_link": "<img src=\"/pics/bookx_time_series.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://www.springer.com/gp/book/9783319524511\">Time Series Analysis and Its Applications</a></h3>",
     "cap_p1": "<p>Great college/graduate level time series analysis book. Easy to read. Great introductory book. The downside is that almost none of the models in this book has application value in real world problems.</p>",
-    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>"
+    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>",
+    "sub": "Stats"
   },
   {
     "img_link": "<img src=\"/pics/bookx_coin.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://oxford.universitypressscholarship.com/view/10.1093/0198774508.001.0001/acprof-9780198774501\">Likelihood-Based Inference in Cointegrated Vector Autoregressive Models</a></h3>",
     "cap_p1": "<p>A classic text on co-integration. It has all the necessary formulae for you to implement you own program for co-integration tests. Highly recommended.</p>",
-    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Econ"
   },
   {
     "img_link": "<img src=\"/pics/bookx_math_stats.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://www.cengage.com/c/mathematical-statistics-and-data-analysis-3e-rice/9780534399429/\">Mathematical Statistics and Data\u00a0Analysis by John A. Rice</a></h3>",
     "cap_p1": "<p>Introductory book on mathematical statistics. It is the book recommended by my advisor on this subject. Easy to read.</p>",
-    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Stats"
   },
   {
     "img_link": "<img src=\"/pics/bookx_stats_inf.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://www.amazon.com/Statistical-Inference-George-Casella/dp/0534243126\">Statistical Inference by Casella and Berger</a></h3>",
     "cap_p1": "<p>Classic graduate level text on statistical inference. Need to say no more.</p>",
-    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Stats"
   },
   {
     "img_link": "<img src=\"/pics/bookx_linear_models.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://www.routledge.com/A-Primer-on-Linear-Models/Monahan/p/book/9781420062014\">A Primer on Linear Models by John F. Monahan</a></h3>",
     "cap_p1": "<p>A good text to read after taking linear algebra. May be read along with statistical inference. The text is a bit dry and boring though.</p>",
-    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>"
+    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>",
+    "sub": "Math"
   },
   {
     "img_link": "<img src=\"/pics/bookx_rudin.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://www.amazon.com/Principles-Mathematical-Analysis-International-Mathematics/dp/007054235X\">Principles of Mathematical Analysis</a></h3>",
     "cap_p1": "<p>Baby Rudin. This book is better studied with a good instructor. There are so many hidden inside stories in this book. An instructor who is only good at copying notes from his/her notepad to the blackboard is no good.</p>",
-    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Math"
   },
   {
     "img_link": "<img src=\"/pics/bookx_pianka.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"http://www.zo.utexas.edu/courses/bio373/ERP-EvolEcol.html\">Evolutionary Ecology by E.R. Pianka</a></h3>",
     "cap_p1": "<p>Best community ecology textbook. The one I read is the 2000 edition. The emphasis is on a conceptual understanding of the ecological processes. There may not be enough math formulae as some of you might want in some of other books, but a conceptual understanding is more important before it can be simplified to some math symbols. </p>",
-    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Ecol"
   },
   {
     "img_link": "<img src=\"/pics/bookx_js_good.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://www.oreilly.com/library/view/javascript-the-good/9780596517748/\">JavaScript: The Good Parts</a></h3>",
     "cap_p1": "<p>This is a controversial book. Many like it, but thankfully most don't. I am glad there is no 2nd edition of this book.</p>",
-    "cap_p2": "<p><span class=\"label label-danger\">Yuck!</span></p>"
+    "cap_p2": "<p><span class=\"label label-danger\">Yuck!</span></p>",
+    "sub": "Tech"
   },
   {
     "img_link": "<img src=\"/pics/bookx_js_YDK.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://github.com/getify/You-Dont-Know-JS\">You Don't Know JS Series by Kyle Simpson</a></h3>",
-    "cap_p1": "<p>Kyle hated <i>JavaScript: The Good Parts</i> so much that he wrote this series. Highly recommended. Easy to read and follow. The whole series is available on GitHub as well.</p>",
-    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+    "cap_p1": "<p>Kyle hated <i>JavaScript: The Good Parts</i> so much that he wrote this series. This series is highly recommended. Easy to read and follow. The whole series is available on GitHub as well.</p>",
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Tech"
   },
   {
     "img_link": "<img src=\"/pics/bookx_matrix_pop_mod.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://global.oup.com/academic/product/matrix-population-models-9780878931217?cc=us&lang=en&\">Matrix Population Models</a></h3>",
     "cap_p1": "<p>For some reason, I have to give this book a top rating. This book is not meant to be read from cover to cover, but as a reference. It has been my go-to reference book when I am working on matrix population models. Whatever your problem, I bet you can find the answer here or at least get a hint at how to solve it.</p>",
-    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Math"
   },
   {
     "img_link": "<img src=\"/pics/bookx_comp_sys.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"http://csapp.cs.cmu.edu/\">Computer Systems: A Programmer's Perspective</a></h3>",
     "cap_p1": "<p>Easy to read and follow. Make sure you finish the labs portion. The hands-on portion is far more important than the cheap talk.</p>",
-    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "CS"
   },
   {
     "img_link": "<img src=\"/pics/bookx_c.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://www.pearson.com/store/p/c-programming-language/P100001672033/9780137460847?creative=543050952131&keyword=&matchtype=b&network=g&device=c&gclid=CjwKCAjwhOyJBhA4EiwAEcJdcaeVAr76f0PqoBRl66YxMTpCo72ufIomRdcAPS0U2xUwU-vQvjAO6xoCuQAQAvD_BwE&gclsrc=aw.ds\">C Programming Language, 2nd edition</a></h3>",
     "cap_p1": "<p>A must have if you ever code in C. A bible doesn't have to be long. It has only 272 pages. Clear and to-the-point writing. </p>",
-    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>"
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Tech"
   },
   {
     "img_link": "<img src=\"/pics/bookx_zin_glmm.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"http://highstat.com/index.php/zero-inflated-models-and-generalized-linear-mixed-models-with-r\">Zero Inflated Models and Generalized Linear Mixed Models</a></h3>",
-    "cap_p1": "<p>A nice introductory book on zero inflated models and GLMMs. It is nice to look at these models from a Bayesian perspective. The reason this book is rated any higher is that it is unavailable from most libraries. When it is available, there is usually just one copy.</p>",
-    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>"
+    "cap_p1": "<p>A nice introductory book on zero inflated models and GLMMs. It is nice to look at these models from a Bayesian perspective. The reason this book is not rated any higher is that it is not stocked by most libraries in the US. When it is stocked, there is usually just one copy and it is already gone.</p>",
+    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>",
+    "sub": "Stats"
+  },
+  {
+    "img_link": "<img src=\"/pics/bookx_ocp.png\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.oreilly.com/library/view/ocp-oracle-certified/9781119067900/\">OCP Oracle Certified Professional Java SE 8 Programmer II</a></h3>",
+    "cap_p1": "<p>Well structured and easy to read and follow. No prior knowledge is required, which is a good thing. You absolutely do not need an instructor to work through this book.</p>",
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Tech"
+  },
+  {
+    "img_link": "<img src=\"/pics/bookx_movement.png\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://peterturchin.com/quantitative-analysis-of-movement/\">Quantitative Analysis of Movement</a></h3>",
+    "cap_p1": "<p>Very well written piece on the analysis of movement.</p>",
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Ecol"
+  },
+  {
+    "img_link": "<img src=\"/pics/bookx_all_of_stats.png\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://link.springer.com/book/10.1007/978-0-387-21736-9\">All of Statistics</a></h3>",
+    "cap_p1": "<p>A \u201cconcise introduction to a larger number of topics\u201d. I use it to look up terms that I am not sure of. You may use Wikipedia, but the wiki page can be too technical and hard to understand sometimes.</p>",
+    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>",
+    "sub": "Stats"
+  },
+  {
+    "img_link": "<img src=\"/pics/bookx_practical_cpp.png\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.oreilly.com/library/view/practical-c-programming/0596004192/\">Practical C++ Programming, 2nd Edition</a></h3>",
+    "cap_p1": "<p>A boring one. I have never managed to make any progress on reading this one. It may have some nice content, but I don't know and don't want to know any more.</p>",
+    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>",
+    "sub": "Tech"
+  },
+  {
+    "img_link": "<img src=\"/pics/bookx_linux.png\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.oreilly.com/library/view/linux-pocket-guide/9781449332969/\">Linux Pocket Guide, 2nd Edition</a></h3>",
+    "cap_p1": "<p>Good to have when you are offline and do not want to read the man page.</p>",
+    "cap_p2": "<p><span class=\"label label-warning\">Good</span></p>",
+    "sub": "Tech"
+  },
+  {
+    "img_link": "<img src=\"/pics/bookx_l_perl.png\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.oreilly.com/library/view/learning-perl-7th/9781491954317/\">Learning Perl, 7th Edition</a></h3>",
+    "cap_p1": "<p>Easy to read. I learned regexp from this book.</p>",
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Tech"
+  },
+  {
+    "img_link": "<img src=\"/pics/bookx_scrape_python.png\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.oreilly.com/library/view/web-scraping-with/9781491910283/\">Web Scraping with Python</a></h3>",
+    "cap_p1": "<p>One of the core python skill sets. </p>",
+    "cap_p2": "<p><span class=\"label label-primary\">Reading</span></p>",
+    "sub": "Tech"
+  },
+  {
+    "img_link": "<img src=\"/pics/bookx_gam.png\" alt=\"book cover\">",
+    "cap_link": "<h3><a href=\"https://www.routledge.com/Generalized-Additive-Models-An-Introduction-with-R-Second-Edition/Wood/p/book/9781498728331\">Generalized Additive Models</a></h3>",
+    "cap_p1": "<p>The go to source for GAMs. I know the <i>mgcv</i> package is handy and you can do most of the analysis without knowing much about GAM. This is the beauty of abstraction, but that does not render this book useless. Highly recommended.</p>",
+    "cap_p2": "<p><span class=\"label label-success\">Awesome!</span></p>",
+    "sub": "Stats"
   }
 ]
--- js/write-books.js
+++ js/write-books.js
@@ -1,10 +1,10 @@
 var len = Object.keys(data).length;
-var text_pre = '<div class="col-sm-6 col-md-4"><div class="thumbnail" style="height: 550px;overflow: auto;">';
+var text_pre = ['<div class="col-sm-6 col-md-4" data-subject="', '"><div class="thumbnail" style="height: 550px;overflow: auto;">'];
 var text_post = '</div></div></div>';
 var text_cap = '<div class="caption">';
 var text = '';
 for(let i=0; i < len; i++){
-  text +=text_pre + data[i].img_link + text_cap + data[i].cap_link 
+  text +=text_pre[0] + data[i].sub + text_pre[1] + data[i].img_link + text_cap + data[i].cap_link 
       + data[i].cap_p2 + data[i].cap_p1 + text_post;
 }
 
