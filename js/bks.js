5afc60304dafea92be81ec4385f114490e9e6bac
hyena <eidotog@gmail.com>
2021-12-01 02:51:51 -0600

    typo fix

--- js/bks.js
+++ js/bks.js
@@ -248,7 +248,7 @@ data=
   {
     "img_link": "<img src=\"/pics/bookx_gnumake.png\" alt=\"book cover\">",
     "cap_link": "<h3><a href=\"https://www.oreilly.com/library/view/managing-projects-with/0596006101/\">Managing Projects with GNU Make, 3rd Edition</a></h3>",
-    "cap_p1": "<p>Typos do run rampant in this book, and the errata page is <a href='https://www.oreilly.com/catalog/errata.csp?isbn=9780596006105' target='_blank'>here</a>. Everything seems to be linked with to everything else. To be able to understand this book, you may need to read it twice just like make would its Makefile.</p>",
+    "cap_p1": "<p>Typos do run rampant, and here is <a href='https://www.oreilly.com/catalog/errata.csp?isbn=9780596006105' target='_blank'>the errata page</a>. Everything seems to be linked to everything else. To fully understand this book, you may need to read it twice just as make would its Makefile.</p>",
     "cap_p2": "<p><span class=\"label label-primary\">Reading</span></p>",
     "sub": "Tech"
   }
