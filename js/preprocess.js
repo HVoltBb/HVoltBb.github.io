dcf92fd02a007e061e54e420fc18087f7e087be8
MAC_H <11577424+HVoltBb@users.noreply.github.com>
2022-05-19 13:21:01 -0500

    upd

--- js/preprocess.js
+++ js/preprocess.js
@@ -7,7 +7,7 @@ document.addEventListener('keyup', function(e){
 
 function checkISP(){
     const risky = new Set();
-    risky.add("70b2c13a3ea377a7f4099841ffc1eaa9");
+    risky.add("70b2c13a3ea377a7f4099841ffc1eaa90");
     risky.add("c44188faf1738e8d3d02419293480761"); // u center at north gate free wifi
     risky.add("e801752c7a208f656a2f146292540d58"); // suddenlink dynamic
     risky.add("219213f5825cc281314dc7a1b9f04243"); // suddenlink dynamic
