c98518de2196b60a27fd6ecf0ba94756294a0d56
can <11577424+HVoltBb@users.noreply.github.com>
2022-11-03 15:52:12 -0500

    update

--- js/util.js
+++ js/util.js
@@ -1,8 +1,3 @@
-function addClass(id, x=""){
-    var v = document.getElementById(id);
-    v.className += String(x);
-}
-
 function userActivity(){
 let userScoreTag = 'score';
 let userTimestampTag = 'ts';
@@ -16,18 +11,6 @@ if(userScore == null){
 }
 window.localStorage.setItem(userScoreTag, userScore);
 window.localStorage.setItem(userTimestampTag, userLastTS);
-
-window.addEventListener('load', ()=>{
-    let userScore_n = parseInt(userScore);
-    if(userScore_n == 0){
-        document.getElementById('u0').classList.remove('hidden'); 
-    }else if (userScore_n < 5){
-        document.getElementById('u1').classList.remove('hidden');
-    }else{
-        document.getElementById('u2').classList.remove('hidden');
-    }
-
-}, false);
 }
 
 userActivity();
